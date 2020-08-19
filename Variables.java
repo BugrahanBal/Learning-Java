1c621784d5e011f1bea3d2778ed6758e32f052e1
BugrahanBal <69077840+BugrahanBal@users.noreply.github.com>
2020-08-19 22:42:19 +0300

    Update and rename Github.java to Variables.java

---
+++ Variables.java
@@ -0,0 +1,27 @@
+
+// If you want to use variable in Java you have to define types of variables
+// For example I would like to assign 5 to b and I am using equal mark but dont forget to define type of variable
+// Integer,int,float,long,double using for numbers
+// String using for words
+// As you can see sum of the string and int at output
+
+
+package com.example.githubproject;
+
+public class Github {
+
+    public static void main(String[] args){
+
+        int a =5;
+        int b =4;
+        String c ="selam";
+        System.out.println(a+c);
+        System.out.println(a+b);
+    }
+}
+
+
+OUTPUT=
+    
+    5selam
+    9
