5e93ac9735928bec998e75847081e6be2c13778d
BugrahanBal <69077840+BugrahanBal@users.noreply.github.com>
2020-08-19 22:30:28 +0300

    Add files via upload

---
+++ Github.java
@@ -0,0 +1,13 @@
+package com.example.githubproject;
+
+public class Github {
+
+    public static void main(String[] args){
+
+        int a =5;
+        int b =4;
+        String c ="selam";
+        System.out.println(a+c);
+        System.out.println(a+b);
+    }
+}
