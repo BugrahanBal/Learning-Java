8d832918f65b1a1d3bebffd5c6fa67a6eecd3d67
BugrahanBal <69077840+BugrahanBal@users.noreply.github.com>
2020-08-20 20:58:16 +0300

    Update and rename Github.java to Structure.java

--- Github.java
+++
@@ -1,9 +0,0 @@
-package com.example.githubproject;
-
-public class Github {
-
-    public static void main(String[] args){
-
-        S
-    }
-}
---
+++ Structure.java
@@ -0,0 +1,25 @@
+package com.example.githubproject;
+
+// what is the package ??
+
+/* package is very usefull method for us because package contains class or many classes,
+these clases are related each of them and package provide organize this class,
+so we have use package :) */
+
+public class Github {
+    
+// I will explain public (and also private and protected but later )
+//If we want to execute codes we should use classes and methods, above this class has Github name and public type
+
+
+    public static void main(String[] args){
+        
+/* We use package,class and turn on methods,If we would like to execute codes in Java we should get used to create class,
+package,methods.We are typing codes in methods,and methods should be in class and classes should be in package 
+respectively */
+        // yes finally we can type codes and execute....
+        
+       //codes codes codes
+        
+    }
+}
